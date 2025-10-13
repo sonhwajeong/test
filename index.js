4343b9c82d22d669de5e048ead73f3f8278e3b96
bunny5758 <bunny5758@bizdata.kr>
2025-10-13 10:51:53 +0900

    55

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // 루트 index.js
 import { registerRootComponent } from 'expo';
-import App from './apps/app/src/App'; // 실제 TSX 엔트리
+import App from './apps/appdata/src/App'; // 실제 TSX 엔트리
 registerRootComponent(App);
